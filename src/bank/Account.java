1bb3b56a0d8b16eb9691a85cf1a016f02ff0057b
waelimrankj-dev <waelimrankj@gmail.com>
2025-12-02 23:31:09 +0300

    Account sınıfı eklendi ve encapsulation uygulandı

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,5 +1,40 @@
 package bank;
 
-public class Account {
+public abstract class Account {
 
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    public Account(String accountNumber, String ownerName, double balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        balance += amount;
+    }
+
+    
+    public abstract void withdraw(double amount);
+
+    protected void setBalance(double amount) {
+        this.balance = amount;
+    }
 }
