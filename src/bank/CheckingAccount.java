81a7263785e541dd478c2c9dd5b8976ebc651e1b
waelimrankj-dev <waelimrankj@gmail.com>
2025-12-20 19:46:47 +0300

    CheckingAccount sınıfına detaylı Javadoc eklendi

--- src/bank/CheckingAccount.java
+++ src/bank/CheckingAccount.java
@@ -1,16 +1,32 @@
 package bank;
 
+/**
+ * CheckingAccount sınıfı.
+ * <p>
+ * Overdraft (eksi bakiye) özelliği olan vadesiz hesap türüdür.
+ */
 public class CheckingAccount extends Account {
 
     private double overdraftLimit;
 
-
+    /**
+     * CheckingAccount constructor.
+     *
+     * @param accountNumber  hesap numarası
+     * @param ownerName      hesap sahibi
+     * @param balance        başlangıç bakiyesi
+     * @param overdraftLimit izin verilen eksi bakiye limiti
+     */
     public CheckingAccount(String accountNumber, String ownerName, double balance, double overdraftLimit) {
         super(accountNumber, ownerName, balance);
         this.overdraftLimit = overdraftLimit;
     }
 
-   
+    /**
+     * Overdraft limitini dikkate alarak para çekme işlemi yapar.
+     *
+     * @param amount çekilecek miktar
+     */
     @Override
     public void withdraw(double amount) {
         if (amount <= 0) {
