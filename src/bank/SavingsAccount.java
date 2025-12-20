3ca3726408044f3cc61c7242f5422a339b6584bf
waelimrankj-dev <waelimrankj@gmail.com>
2025-12-20 19:41:06 +0300

    SavingsAccount sınıfına detaylı Javadoc eklendi

--- src/bank/SavingsAccount.java
+++ src/bank/SavingsAccount.java
@@ -1,17 +1,32 @@
 package bank;
 
-
+/**
+ * SavingsAccount sınıfı.
+ * <p>
+ * Faiz kazancı sağlayan tasarruf hesabını temsil eder.
+ */
 public class SavingsAccount extends Account {
 
     private double interestRate;
 
-
+    /**
+     * SavingsAccount constructor.
+     *
+     * @param accountNumber hesap numarası
+     * @param ownerName     hesap sahibi
+     * @param balance       başlangıç bakiyesi
+     */
     public SavingsAccount(String accountNumber, String ownerName, double balance) {
         super(accountNumber, ownerName, balance);
         this.interestRate = 0.02;
     }
 
-
+    /**
+     * Hesaptan para çekme işlemi.
+     * Yetersiz bakiye durumunda hata fırlatır.
+     *
+     * @param amount çekilecek miktar
+     */
     @Override
     public void withdraw(double amount) {
         if (amount <= 0) {
@@ -23,7 +38,9 @@ public class SavingsAccount extends Account {
         setBalance(getBalance() - amount);
     }
 
-
+    /**
+     * Hesaba aylık faiz uygular.
+     */
     public void applyMonthlyInterest() {
         double interest = getBalance() * interestRate;
         setBalance(getBalance() + interest);
